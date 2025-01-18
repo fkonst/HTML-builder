6b1d814cf77c5daa71765160b296b76ce11265c4
Konstantin F <kf4654569@gmail.com>
2025-01-18 23:40:09 +0300

    feat: add 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs/promises");
+
+const getStyles = () => {
+    const styles = path.join(__dirname, "styles");
+    return fsPromises.readdir(styles, { withFileTypes: true})
+                     .then(files => files.filter(file => file.isFile() && path.extname(file.name) === ".css"))
+}
+
+const writeBundle = (styles) => {
+    const bundlePath = path.join(__dirname, "project-dist", "bundle.css");
+    const writeStream = fs.createWriteStream(bundlePath);
+    for(let file of styles) {
+        const filePath = path.join(file.parentPath, file.name);
+        const readStream = fs.createReadStream(filePath);
+        readStream.on("data", data => writeStream.write(data))
+        
+    }
+}
+
+(async () => {
+    const styles = await getStyles();
+    writeBundle(styles);
+})()
